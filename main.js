71d6c912a8fb6d8975bd3814db46744fe9a88937
ninasjoberg <nina.sjoberg@gmail.com>
2017-02-17 12:29:54 +0100

    försök på eventListerners som misslyckats

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 
 
 
+var iconEvent = document.getElementsByClassName("event")[2];
+console.log(iconEvent);
+
+iconEvent.addEventListener('click', function(event){
+	event.target.classList.toggle('visible');
+	console.log(event.target);
+})
+
+
+/*
+function change_boxes() {
+    var boxes = document.getElementsByClassName('boxes'),
+        i = boxes.length;
+
+    while(i--) {
+        boxes[i].style.backgroundColor = "green";
+    }
+}
+*/
+/*
+var button = document.querySelectorAll("button")[0];
+button.addEventListener('click', function() {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}, false);
+*/
+
+
+
+/*
 function toggleActive(info){
 	var nowActive = document.getElementsByClassName('active');
 	  		console.log(nowActive);
@@ -17,3 +51,4 @@ function toggleActive(info){
 }
 
 console.log('hej');
+*/
